f6b5921cc87a1f2b42822029a1627f91b0a20e6d
billta <hao.tang@alcatel-lucent.com>
2014-01-29 15:48:22 -0800

    pa3 done

--- coursera/cda/pa3.R
+++ coursera/cda/pa3.R
@@ -73,18 +73,81 @@ xyplot(death ~ npatient | owner, group=owner, xlab='number of patients seen', yl
 #
 # PA3.5
 #
-
 best<-function(state,outcome) {
     if (outcome=='heart attack') { i <-11 }
     else if (outcome=='heart failure') { i <-17 }
     else if (outcome=='pneumonia') { i <-23 }
     else { stop('invalid name') }
     mydata<-read.csv('outcome-of-care-measures.csv', colClasses='character')
-    states<-unique(mydata$State)
-    if (is.element(state, states)) {
+    mystates<-unique(mydata$State)
+    if (is.element(state, mystates)) {
         mydata2<-mydata[ mydata$State == state , c(2,7,i)]  #Name, State, itemSelected
-        print (head( mydata2[with( mydata2, order (3,1))] , n=1 )$Hospital.Name)
+        mydata2[,3]<- suppressWarnings( as.numeric(mydata2[,3]) )
+        mydata3 <- mydata2[ complete.cases(mydata2), ]
+        mydata4 <-  mydata3[ order( mydata3[,3]), ]
+        print (head( mydata4$Hospital.Name, n=1))
     } else { stop('invalid state') }  			      
 }
-
-
+#
+# PA3.6
+#
+rankhospital<-function(state, outcome, num='best') {
+    if (outcome=='heart attack') { i <-11 }
+    else if (outcome=='heart failure') { i <-17 }
+    else if (outcome=='pneumonia') { i <-23 }
+    else { stop('invalid outcome') }
+    mydata<-read.csv('outcome-of-care-measures.csv', colClasses='character')
+    mystates<-unique(mydata$State)
+    if (is.element(state, mystates)) {
+        mydata2<-mydata[ mydata$State == state , c(2,7,i)]  #Name, State, itemSelected
+        mydata2[,3]<- suppressWarnings( as.numeric(mydata2[,3]) )
+        mydata3 <-  mydata2[ complete.cases(mydata2), ] 
+        mydata4 <-  mydata3[ order( mydata3[,3], mydata3[,1]), ]
+        if (num=='best') {
+            print (head( mydata4$Hospital.Name, n=1))
+        } else if (num=='worst') {
+            print (tail( mydata4$Hospital.Name, n=1))
+        } else {
+            #mynum<-suppressWarnings(as.numeric(num))
+            mynum<-as.numeric(num)
+            print (mydata4$Hospital.Name[mynum])
+        }
+        ## mydata2<-mydata[ mydata$State == state , c(2,7,i)]  #Name, State, itemSelected
+        ## mydata2[,3]<- suppressWarnings( as.numeric(mydata2[,3]) )
+        ## mydata3 <- mydata2[ complete.cases(mydata2), ]
+        ## mydata3$Ranking<- ave( mydata3$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack, mydata3$State, FUN=rank)
+        ## # mydata4<- mydata3[ order(-1* as.numeric(num), 
+        ## return( mydata3 )
+    } else { stop('invalid state') }  			      
+}
+#
+# PA3.7
+#
+rankall<-function(outcome, num='best') {
+    if (outcome=='heart attack') { i <-11 }
+    else if (outcome=='heart failure') { i <-17 }
+    else if (outcome=='pneumonia') { i <-23 }
+    else { stop('invalid outcome') }
+    
+    mydata<-read.csv('outcome-of-care-measures.csv', colClasses='character')
+    mystates<-unique(mydata$State)
+    bar <- data.frame( character(), character() )
+    for(state in sort(mystates)) {
+        mydata2<-mydata[ mydata$State == state , c(2,7,i)]  #Name, State, itemSelected
+        mydata2[,3]<- suppressWarnings( as.numeric(mydata2[,3]) )
+        mydata3 <-  mydata2[ complete.cases(mydata2), ] 
+        mydata4 <-  mydata3[ order( mydata3[,3], mydata3[,1]), ]
+        if (num=='best') {
+            foo<-mydata4[ 1, c(2,1) ]
+        } else if (num=='worst') {
+            foo<-mydata4[ nrow(mydata4), c(2,1) ]
+        } else {
+            mynum<-as.numeric(num)
+            foo<-mydata4[num, c(2,1) ]
+        }
+        bar<-rbind( bar,foo )
+    }
+    names(bar)[1]<-'state'
+    names(bar)[2]<-'hospital'
+    return (bar)
+}
